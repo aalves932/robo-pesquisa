2fb91d6e42924c621d930633d70de1c98c8bcecd
André Luis Alves <andre.alves@sixsigma.com.br>
2020-08-03 11:26:34 -0300

    update

--- gTrends.js
+++ gTrends.js
@@ -1,7 +1,7 @@
 const googleTrends = require('google-trends-api');
 const Parser = require('rss-parser');
 
-googleTrends.autoComplete({keyword: 'Back to School'})
+googleTrends.autoComplete({keyword: 'LInguagem de Programação'})
 .then(function(results) {
     test= JSON.parse(results);
     console.log(test.default.topics);
